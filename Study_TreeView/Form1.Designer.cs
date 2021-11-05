31f05fb19fe4d247a3e3e25246836612fcd26103
HUIKIM0 <fehs123@naver.com>
2021-11-05 18:09:45 +0900

    Initial commit

---
+++ Study_TreeView/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace Study_TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tboxLocation = new System.Windows.Forms.TextBox();
+            this.btnTreeLoad = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tboxBackUp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExtend = new System.Windows.Forms.Button();
+            this.btnCollapse = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lboxCommand = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lboxFile = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lboxLog = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tboxBackUp);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnTreeLoad);
+            this.groupBox1.Controls.Add(this.tboxLocation);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(765, 61);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setting";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.lboxFile);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.lboxCommand);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.treeView1);
+            this.groupBox2.Controls.Add(this.btnCollapse);
+            this.groupBox2.Controls.Add(this.btnExtend);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(13, 79);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(765, 355);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Location";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "경로";
+            // 
+            // tboxLocation
+            // 
+            this.tboxLocation.Location = new System.Drawing.Point(76, 23);
+            this.tboxLocation.Name = "tboxLocation";
+            this.tboxLocation.Size = new System.Drawing.Size(230, 25);
+            this.tboxLocation.TabIndex = 1;
+            // 
+            // btnTreeLoad
+            // 
+            this.btnTreeLoad.Location = new System.Drawing.Point(312, 24);
+            this.btnTreeLoad.Name = "btnTreeLoad";
+            this.btnTreeLoad.Size = new System.Drawing.Size(43, 25);
+            this.btnTreeLoad.TabIndex = 2;
+            this.btnTreeLoad.Text = "T";
+            this.btnTreeLoad.UseVisualStyleBackColor = true;
+            this.btnTreeLoad.Click += new System.EventHandler(this.btnTreeLoad_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "백업폴더 경로";
+            // 
+            // tboxBackUp
+            // 
+            this.tboxBackUp.Location = new System.Drawing.Point(501, 23);
+            this.tboxBackUp.Name = "tboxBackUp";
+            this.tboxBackUp.Size = new System.Drawing.Size(230, 25);
+            this.tboxBackUp.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TreeNode";
+            // 
+            // btnExtend
+            // 
+            this.btnExtend.Location = new System.Drawing.Point(136, 23);
+            this.btnExtend.Name = "btnExtend";
+            this.btnExtend.Size = new System.Drawing.Size(83, 25);
+            this.btnExtend.TabIndex = 5;
+            this.btnExtend.Text = "Extend";
+            this.btnExtend.UseVisualStyleBackColor = true;
+            this.btnExtend.Click += new System.EventHandler(this.btnExtend_Click);
+            // 
+            // btnCollapse
+            // 
+            this.btnCollapse.Location = new System.Drawing.Point(225, 24);
+            this.btnCollapse.Name = "btnCollapse";
+            this.btnCollapse.Size = new System.Drawing.Size(83, 25);
+            this.btnCollapse.TabIndex = 6;
+            this.btnCollapse.Text = "Collapse";
+            this.btnCollapse.UseVisualStyleBackColor = true;
+            this.btnCollapse.Click += new System.EventHandler(this.btnCollapse_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(11, 55);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(297, 285);
+            this.treeView1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "▶";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(393, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Command";
+            // 
+            // lboxCommand
+            // 
+            this.lboxCommand.FormattingEnabled = true;
+            this.lboxCommand.ItemHeight = 15;
+            this.lboxCommand.Location = new System.Drawing.Point(396, 53);
+            this.lboxCommand.Name = "lboxCommand";
+            this.lboxCommand.Size = new System.Drawing.Size(348, 79);
+            this.lboxCommand.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(393, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "File";
+            // 
+            // lboxFile
+            // 
+            this.lboxFile.FormattingEnabled = true;
+            this.lboxFile.ItemHeight = 15;
+            this.lboxFile.Location = new System.Drawing.Point(396, 171);
+            this.lboxFile.Name = "lboxFile";
+            this.lboxFile.Size = new System.Drawing.Size(348, 169);
+            this.lboxFile.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(559, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "▼";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(674, 436);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(83, 25);
+            this.btnCopy.TabIndex = 14;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lboxLog);
+            this.groupBox3.Location = new System.Drawing.Point(13, 456);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(765, 124);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Log";
+            // 
+            // lboxLog
+            // 
+            this.lboxLog.FormattingEnabled = true;
+            this.lboxLog.ItemHeight = 15;
+            this.lboxLog.Location = new System.Drawing.Point(6, 25);
+            this.lboxLog.Name = "lboxLog";
+            this.lboxLog.Size = new System.Drawing.Size(738, 94);
+            this.lboxLog.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 592);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTreeLoad;
+        private System.Windows.Forms.TextBox tboxLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tboxBackUp;
+        private System.Windows.Forms.ListBox lboxCommand;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnCollapse;
+        private System.Windows.Forms.Button btnExtend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox lboxFile;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox lboxLog;
+    }
+}
+
---
+++ Study_TreeView/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Study_TreeView
+{
+    public partial class Form1 : Form
+    {
+
+        // Log Level 지정
+        enum enLogLevel
+        {
+            Info,
+            Warning,
+            Error,
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //TreeNode 경로 
+        private void btnTreeLoad_Click(object sender, EventArgs e)
+        {
+            TreeViewLoadPath(treeView1, tboxLocation.Text);
+        }
+
+
+        // 선택한 Node의 하위노드를 펼치기
+        private void btnExtend_Click(object sender, EventArgs e)
+        {
+            if(treeView1.SelectedNode != null)
+            {
+                treeView1.SelectedNode.ExpandAll();       //treeView1.ExpandAll();   다 펼치기
+            }
+        }
+
+        //TreeNode 전체 닫기
+
+        private void btnCollapse_Click(object sender, EventArgs e)
+        {
+            treeView1.CollapseAll();
+        }
+
+
+        #region TreeView Node Funtion Set
+
+        //TreeView를 그리는 함수
+        private void TreeViewLoadPath(TreeView treeView, string path)
+        {
+
+            treeView.Nodes.Clear();   //중복되서 찍히기 않게 기존의 TreeView 초기화
+
+            DirectoryInfo rootDirectoryInfo = new DirectoryInfo(path);  // DirectoryInfo class 선언
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));  // 다 합체된 완성된 폴더를 Add
+        }
+
+
+        // 경로에 따른 폴더 찾는 함수
+        // 하위폴더 3번째 -> 하위폴더 2번째 -> 하위폴더 1번째 -> Root
+        // 아래에서 부터 위로 합체되서 올라간다는 느낌(재귀함수)
+        private TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            TreeNode directoryNode = new TreeNode(directoryInfo.Name);
+
+
+            //경로에 포함되는 폴더 찾기 foreach
+            foreach (var directory in directoryInfo.GetDirectories())   // 해당 경로의 디렉토리를 배열로 가져옴
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));   // 경로를 재귀 함수로 계속 호출 하면서 하위 노드 들을 찾아 옴
+
+
+            // File 을 가지고 오기 위한 Node
+            //foreach (var file in directoryInfo.GetFiles())
+            //    directoryNode.Nodes.Add(new TreeNode(file.Name));
+
+
+            return directoryNode;
+        }
+        #endregion
+
+
+        #region Log OverLoading
+
+
+        #endregion
+
+    }
+}
